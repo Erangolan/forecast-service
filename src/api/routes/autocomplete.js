f4d99c1061abc0834f8b4a25478505902fcc9d01
Erangolan <37651948+Erangolan@users.noreply.github.com>
2022-03-31 15:50:03 +0300

    added autocomplete route

--- src/api/routes/autocomplete.js
+++ src/api/routes/autocomplete.js
@@ -1,7 +1,40 @@
 const router = require('express').Router()
+const axios = require('axios')
+const {
+  API_KEY,
+  LOCATION_API_AUTOCOMPLETE,
+} = require('../../consts')
 
 router.get('/', async (req, res) => {
-  res.send('Hello World!')
+  const {
+    query: {
+      text,
+    },
+  } = req
+
+  try {
+    if (!text) {
+      throw new Error('invalid query')
+    }
+
+    console.log('fetching autocompleting cities')
+
+    const {
+      data,
+    } = await axios(`${LOCATION_API_AUTOCOMPLETE}?q=${text}&apikey=${API_KEY}`)
+
+    console.log('first api request succeed')
+
+    return res.json({
+      cities: data,
+    })
+  } catch (e) {
+    console.log({ stack: e.stack }, 'error with autocomplete route', { message: e.toString() })
+
+    return res.status(500).json({
+      error: e,
+    })
+  }
 })
 
 module.exports = router

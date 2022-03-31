12f0f12ca40794a1aaad96278d3fb2f8c21f0a4b
Erangolan <37651948+Erangolan@users.noreply.github.com>
2022-03-31 15:26:15 +0300

    more initializations

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST')
+    return res.status(200).json({})
+  }
+  return next()
+})
+
 app.use('/api', require('./api'))
 
 app.listen(3000, () => console.log('server listening on port 3000'))
